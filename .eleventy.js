942af50bd646a6d317a49ca1f45b496cc4ee3321
Carey <mbt.carey@gmail.com>
2025-12-14 13:35:54 -0600

    Change name of project

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
-module.exports = function(eleventyConfig) {
-    return {
-        dir: {
-            input: "src",
-          },
-        pathPrefix: "/ctcgallery/"
-    }
+module.exports = function (eleventyConfig) {
+  return {
+    dir: {
+      input: "src",
+    },
+    pathPrefix: "/particledemo/",
   };
+};
